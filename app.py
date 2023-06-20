f53dbdbebe1b339a9c3b03bdb15d46e2a5bc1e44
Greg-Art <gregoryarthur98@gmail.com>
2023-06-20 16:44:52 -0500

    update

--- app.py
+++ app.py
@@ -2,5 +2,25 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
+from prophet import Prophet
 
-st.write(""" # A Timeseries Forecasting App for Favorita Corporation""")
+##adding my title
+
+st.title("This is a Time Forecasting App with Facebook Prophet")
+
+##adding my description 
+
+st.markdown("This app predicts daily sales across all Favorita Stores")
+
+###importing my dataset
+df= pd.read_csv("models/fbtest.csv")
+
+df.head()
+df.info()
+
+##importing my model 
+st.subheader("This is a sample dataframe")
+st.dataframe(df.head())
+
+##creating a plot with our time series 
+
