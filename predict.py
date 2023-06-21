0f7375703b60ec1bf3d18e405af0ffb32b4ee285
Greg-Art <gregoryarthur98@gmail.com>
2023-06-21 11:07:25 -0500

    update

--- predict.py
+++ predict.py
@@ -15,13 +15,13 @@ st.markdown("This app predicts daily sales across all Favorita Stores")
 st.header("Prediction with Facebook Prophet")
 
 ## a plot of the Daily sales for favorita
-df_ori= pd.read_csv("models/original_dataframe.csv")
+df_ori= pd.read_csv("dataframes/original_dataframe.csv")
 df_ori= df_ori.set_index("date")
 st.subheader("A Chart of the Daily Sales Across Favorita Stores")
 st.line_chart(df_ori["sales"])
 
 ##showing the user a sample dataframe they should have 
-df_sample= pd.read_csv("models/test.csv")
+df_sample= pd.read_csv("dataframes/test.csv")
 df_sample= df_sample.drop("Unnamed: 0", axis=1)
 st.subheader("This should be the format of your input: y is your sales, ds is your date")
 st.dataframe(df_sample.head())
