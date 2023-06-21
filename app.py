523c92cdec9c10a321f064ca28c8d4049f202727
Greg-Art <gregoryarthur98@gmail.com>
2023-06-21 03:50:32 -0500

    update

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ st.markdown("This app predicts daily sales across all Favorita Stores")
 
 ## a plot of the Daily sales for favorita
 df= pd.read_csv("models/original_dataframe.csv")
+df= df.set_index("date")
 st.subheader("A Chart of the Daily Sales Across Favorita Stores")
 st.line_chart(df["sales"])
 
@@ -24,4 +25,11 @@ st.line_chart(df["sales"])
 df_sample= pd.read_csv("models/sample.csv")
 df_sample= df_sample.drop("Unnamed: 0", axis= 1)
 st.subheader("This should be the format of your input: y is your sales, ds is your date")
-st.dataframe(df_sample.head())
+st.dataframe(df_sample)
+
+##model deployment
+
+st.header("Prediction with Facebook Prophet")
+
+##data encoding
+
