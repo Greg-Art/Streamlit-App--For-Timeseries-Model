1da0ec81c6658c38190e6cdd90c50e597c1b592a
Greg-Art <gregoryarthur98@gmail.com>
2023-06-21 01:51:10 -0500

    update

--- app.py
+++ app.py
@@ -13,14 +13,15 @@ st.title("This is a Time Forecasting App with Facebook Prophet")
 st.markdown("This app predicts daily sales across all Favorita Stores")
 
 ###importing my dataset
-df= pd.read_csv("models/fbtest.csv")
 
-df.head()
-df.info()
 
-##importing my model 
-st.subheader("This is a sample dataframe")
-st.dataframe(df.head())
+## a plot of the Daily sales for favorita
+df= pd.read_csv("models/original_dataframe.csv")
+st.subheader("A Chart of the Daily Sales Across Favorita Stores")
+st.line_chart(df["sales"])
 
-##creating a plot with our time series 
-
+##showing the user a sample dataframe they should have 
+df_sample= pd.read_csv("models/sample.csv")
+df_sample= df_sample.drop("Unnamed: 0", axis= 1)
+st.subheader("This should be the format of your input: y is your sales, ds is your date")
+st.dataframe(df_sample.head())
