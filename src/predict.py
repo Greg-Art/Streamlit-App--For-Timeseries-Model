59c79f60d0b1b8d3c87a05045336aa23343c18cc
Greg-Art <gregoryarthur98@gmail.com>
2023-06-23 04:46:20 -0500

    update

--- src/predict.py
+++ src/predict.py
@@ -1,27 +1,44 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import pickle as pk 
+import joblib 
+
+##loading my model again
+
+model= joblib.load("C:/Users/Gregory Arthur/Desktop/models/fbpmodel.joblib")
+
+##loading my test data
+
+test=pd.read_csv("dataframes/test.csv")
+
+test=test.drop(["holiday", "locale", "transferred"], axis= 1)
+
+result= model.predict(test)
+
+##creating my page 
 
 
-##adding my title
+def front_page():
+    ##adding my title
+    st.title("This is a Time Forecasting App with Facebook Prophet")
+    ##adding my description 
+    st.markdown("This app predicts daily sales across all Favorita Stores")
+    
+    st.header("Prediction with Facebook Prophet")
+    ## a plot of the Daily sales for favorita
+    df_ori= pd.read_csv("dataframes/original_dataframe.csv")
+    df_ori= df_ori.set_index("date")
+    st.subheader("A Chart of the Daily Sales Across Favorita Stores")
+    st.line_chart(df_ori["sales"])
+    
+    ##showing the user a sample dataframe they should have 
+    df_sample= pd.read_csv("dataframes/test.csv")
+    df_sample= df_sample.drop("Unnamed: 0", axis=1)
+    st.subheader("Format of Input: y is your sales, ds is your date")
+    st.dataframe(df_sample.head())
 
-st.title("This is a Time Forecasting App with Facebook Prophet")
 
-##adding my description 
 
-st.markdown("This app predicts daily sales across all Favorita Stores")
 
-st.header("Prediction with Facebook Prophet")
 
-## a plot of the Daily sales for favorita
-df_ori= pd.read_csv("dataframes/original_dataframe.csv")
-df_ori= df_ori.set_index("date")
-st.subheader("A Chart of the Daily Sales Across Favorita Stores")
-st.line_chart(df_ori["sales"])
 
-##showing the user a sample dataframe they should have 
-df_sample= pd.read_csv("dataframes/test.csv")
-df_sample= df_sample.drop("Unnamed: 0", axis=1)
-st.subheader("Format of Input: y is your sales, ds is your date")
-st.dataframe(df_sample.head())
