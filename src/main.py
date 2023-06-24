20cd2e7d3194f0e846383434cc8590647ece7ad2
Greg-Art <gregoryarthur98@gmail.com>
2023-06-24 17:41:32 -0500

    update

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("This is the Main Page")
+st.sidebar.success("Please Select the Page")
+
--- src/pages/main.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.sidebar.success("Please Select the Page")
