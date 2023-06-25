e7fa1276bc18775b1f829eaf84ea80c08005fccc
Greg-Art <gregoryarthur98@gmail.com>
2023-06-25 02:41:59 -0500

    update

---
+++ src/Welcome.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Welcome to Greg's Time Series Forecast App", 
+                   page_icon=""
+                   )
+
+st.title("Welcome to My Homepage")
+st.sidebar.success("Please Select the Page")
+
--- src/pages/main.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Welcome to My Homepage")
-st.sidebar.success("Please Select the Page")
-
