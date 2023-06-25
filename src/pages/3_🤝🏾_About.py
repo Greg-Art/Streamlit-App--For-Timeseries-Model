d3a3c908e830fd1bf1962c3d63d21a24999d3709
Greg-Art <gregoryarthur98@gmail.com>
2023-06-25 05:08:46 -0500

    update

--- src/pages/3_🤝🏾_About.py
+++ src/pages/3_🤝🏾_About.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Learn More About Me:")
+
+st.write("""
+Gregory Arthur is a relentless explorer of the boundless possibilities hidden within data. As a distinguished data 
+analysis trainee at Azubi Africa, my arsenal is fortified with the formidable tools of Excel, Power BI, SQL,
+ and Python, not only that, I am experienced in leveraging frameworks such as Streamlit and Gradio to 
+ develop interactive web app for your machine learning models, which I can deploy on either Heiroku or Hugging Face,
+enabling me to dissect and decipher intricate datasets with finesse. 
+""")
